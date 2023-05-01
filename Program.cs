bf1f7af4f35a35f4af1f52d800ed64a7f71cfedc
Denis <oumniy@gmail.com>
2023-05-01 20:44:38 +0300

    задача о днях недели

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите цифру:");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 6 | number == 7) {
+    System.Console.WriteLine("Да");}
+else {
+    System.Console.WriteLine("нет");
+}
